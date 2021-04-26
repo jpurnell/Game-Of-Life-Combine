0e60f101affe70752d4f9798b225cfda0792dc7c
Justin Purnell <jpurnell@mac.com>
2021-04-26 19:35:04 -0400

    Initial Commit

---
+++ Shared/Game_Of_Life_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_Of_Life_CombineApp.swift
+//  Shared
+//
+//  Created by Justin Purnell on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Game_Of_Life_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
